00674db0ff2ab27d33b0c03914428a41da7da2e7
duancoin <duanjm104@163.com>
2016-07-30 12:59:10 +0800

    段锦梦

--- postnet/best-charge-spec.js
+++ postnet/best-charge-spec.js
@@ -101,7 +101,7 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
+    it('should barcodeChangeToPostcode：两边都是"|"：', ()=> {
         const barcode = '||:|:::|:|:|:::|:::||::||::|:|:|';
 
         let allCodes = loadAllCodes();
@@ -113,7 +113,7 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
+    it('should barcodeChangeToPostcode: 左边非"|": ', ()=> {
         const barcode = ':|:|:::|:|:|:::|:::||::||::|:|:|';
 
         let allCodes = loadAllCodes();
@@ -125,7 +125,7 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
+    it('should barcodeChangeToPostcode: 两边都不是"|" ', ()=> {
         const barcode = ':|:|:::|:|:|:::|:::||::||::|:|::';
 
         let allCodes = loadAllCodes();
@@ -137,7 +137,19 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
+    it('should barcodeChangeToPostcode：长度为32', ()=> {
+        const barcode = '||:|:::|:|:|:::|:::||::||::|:|:|';
+
+        let allCodes = loadAllCodes();
+
+        let postcode = barcodeChangeToPostcode(barcode, allCodes);
+
+        const expected = '95713';
+
+        expect(postcode).toEqual(expected);
+    });
+
+    it('should barcodeChangeToPostcode: 长度为31', ()=> {
         const barcode = '||:|:::|:|:|:::|:::||::||::|:||';
 
         let allCodes = loadAllCodes();
@@ -149,19 +161,7 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '||:|:::|:|:|:::|:::||::||::|:||';
-
-        let allCodes = loadAllCodes();
-
-        let postcode = barcodeChangeToPostcode(barcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(postcode).toEqual(expected);
-    });
-
-    it('should barcodeChangeToPostcode', ()=> {
+    it('should barcodeChangeToPostcode：长度为33：', ()=> {
         const barcode = '||:|:::|:|:|:::|:::||::||::|:|::|';
 
         let allCodes = loadAllCodes();
@@ -173,7 +173,7 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
+    it('should barcodeChangeToPostcode：长度为52：', ()=> {
         const barcode = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|';
 
         let allCodes = loadAllCodes();
@@ -185,7 +185,7 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
+    it('should barcodeChangeToPostcode：长度为51：', ()=> {
         const barcode = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||::|';
 
         let allCodes = loadAllCodes();
@@ -197,7 +197,7 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
+    it('should barcodeChangeToPostcode：长度为53：', ()=> {
         const barcode = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||::::|';
 
         let allCodes = loadAllCodes();
@@ -209,8 +209,20 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '||||||:|:|:|:::|:::||::||::|:|:|';//内含错误编码
+    it('should barcodeChangeToPostcode：正确编码2"|",3":" ', ()=> {
+        const barcode = '||:|:::|:|:|:::|:::||::||::|:|:|';
+
+        let allCodes = loadAllCodes();
+
+        let postcode = barcodeChangeToPostcode(barcode, allCodes);
+
+        const expected = '95713';
+
+        expect(postcode).toEqual(expected);
+    });
+
+    it('should barcodeChangeToPostcode：包含编码5"|",0":" ', ()=> {
+        const barcode = '||||||:|:|:|:::|:::||::||::|:|:|';
 
         let allCodes = loadAllCodes();
 
@@ -221,8 +233,8 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '||||:||:|:|:|:::|:::||::||::|:|:|';//内含错误编码
+    it('should barcodeChangeToPostcode：包含编码4"|",1":" ', ()=> {
+        const barcode = '||||:||:|:|:|:::|:::||::||::|:|:|';
 
         let allCodes = loadAllCodes();
 
@@ -233,8 +245,8 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '|||::|:|:|:|:::|:::||::||::|:|:|';//内含错误编码
+    it('should barcodeChangeToPostcode：包含编码3"|",2":" ', ()=> {
+        const barcode = '||||:::|:|:|:::|:::||::||::|:|:|';
 
         let allCodes = loadAllCodes();
 
@@ -245,8 +257,8 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '|1235784596321458965123547jdteh|';//内含错误编码
+    it('should barcodeChangeToPostcode：包含编码1"|",4":" ', ()=> {
+        const barcode = '|:|::::|:|:|:::|:::||::||::|:|:|';
 
         let allCodes = loadAllCodes();
 
@@ -257,8 +269,8 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '||:::::|:|:|:::|:::||::||::|:|:|';//内含错误编码
+    it('should barcodeChangeToPostcode：包含编码0"|",5":" ', ()=> {
+        const barcode = '|::::::|:|:|:::|:::||::||::|:|:|';
 
         let allCodes = loadAllCodes();
 
@@ -269,8 +281,8 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '|::::::|:|:|:::|:::||::||::|:|:|';//内含错误编码
+    it('should barcodeChangeToPostcode：包含除"|"和":"之外的其他符号"', ()=> {
+        const barcode = '|:::::$|:|:|:::|:::|#::||::|:|:|';
 
         let allCodes = loadAllCodes();
 
@@ -281,8 +293,8 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '|:|::|:|:|:||::L:|:|::||::a::||::|:|::||::|::|||:::|';//有字母
+    it('should barcodeChangeToPostcode：内含字母', ()=> {
+        const barcode = '|:|::|:|:|:||::L:|:|::||::a::||::|:|::||::|::|||:::|';
 
         let allCodes = loadAllCodes();
 
@@ -293,8 +305,8 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '|:|::|:|:|:||::×:|:|::||::#::||::|:|::||::|::|||:::|';//有其他字符
+    it('should barcodeChangeToPostcode：包含数字', ()=> {
+        const barcode = '|9:|:::3:|:|:::|:::||::|2::|:|:|';
 
         let allCodes = loadAllCodes();
 
@@ -305,20 +317,20 @@ describe('postnet', function () {
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '|:|::|:|:|:||::2:|:|::||::0::||::|:|::||::|::|||:::|';//有数字
+    it('should barcodeChangeToPostcode：所有编码和校验码总和不是10的倍数', ()=> {
+        const barcode = '|::||::|:|:|:::|:|:|:::|:||::|:|';
 
         let allCodes = loadAllCodes();
 
         let postcode = barcodeChangeToPostcode(barcode, allCodes);
 
-        const expected = 'Please enter the correct barcode';
+        const expected = '35752';
 
         expect(postcode).toEqual(expected);
     });
 
-    it('should barcodeChangeToPostcode', ()=> {
-        const barcode = '||:|:::|:|:|:::|:::||::||::|:：:|';//校验位错误
+    it('should barcodeChangeToPostcode：校验码错误，所有编码和校验码总和不是10的倍数', ()=> {
+        const barcode = '||:|:::|:|:|:::|:::||::||::|:：:|';
 
         let allCodes = loadAllCodes();
 
@@ -357,103 +369,7 @@ describe('postnet', function () {
         expect(barcode).toEqual(expected);
     });
 
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '45056-1234';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '4505-61234';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '45-5-61234';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '4505461234';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '450561-234';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '45-0561234';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '45056234';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '15935784625';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
+    it('should postcodeChangeToBarcode：正确长度：5位', () =>{
         const postcode = '03542';
 
         let allCodes = loadAllCodes();
@@ -465,7 +381,7 @@ describe('postnet', function () {
         expect(barcode).toEqual(expected);
     });
 
-    it('should postcodeChangeToBarcode', () =>{
+    it('should postcodeChangeToBarcode：长度错误：4位', () =>{
         const postcode = '0352';
 
         let allCodes = loadAllCodes();
@@ -477,7 +393,7 @@ describe('postnet', function () {
         expect(barcode).toEqual(expected);
     });
 
-    it('should postcodeChangeToBarcode', () =>{
+    it('should postcodeChangeToBarcode：长度错误：6位', () =>{
         const postcode = '124630';
 
         let allCodes = loadAllCodes();
@@ -489,7 +405,128 @@ describe('postnet', function () {
         expect(barcode).toEqual(expected);
     });
 
-    it('should postcodeChangeToBarcode', () =>{
+    it('should postcodeChangeToBarcode： 正确长度：9位', () =>{
+        const postcode = '136842796';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = '|:::||::||::||::|::|::|::|::|:||:::||:|:::||::|';
+
+        expect(barcode).toEqual(expected);
+    });
+
+    it('should postcodeChangeToBarcode： 正确长度：10位', () =>{
+        const postcode = '45056-1234';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|';
+
+        expect(barcode).toEqual(expected);
+    });
+
+    it('should postcodeChangeToBarcode：长度错误：8位', () =>{
+        const postcode = '84627061';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = 'Please enter the correct barcode';
+
+        expect(barcode).toEqual(expected);
+    });
+
+    it('should postcodeChangeToBarcode：长度错误：11位', () =>{
+        const postcode = '15935784625';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = 'Please enter the correct barcode';
+
+        expect(barcode).toEqual(expected);
+    });
+
+    it('should postcodeChangeToBarcode: "-"的个数, 正确1个', () =>{
+        const postcode = '45056-1234';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|';
+
+        expect(barcode).toEqual(expected);
+    });
+
+    it('should postcodeChangeToBarcode： "-"的个数（长度位10的）, 错误0个', () =>{
+        const postcode = '0825461934';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = 'Please enter the correct barcode';
+
+        expect(barcode).toEqual(expected);
+    });
+
+    it('should postcodeChangeToBarcode： "-"的个数（长度位10的）, 错误2个', () =>{
+        const postcode = '12-5-61934';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = 'Please enter the correct barcode';
+
+        expect(barcode).toEqual(expected);
+    });
+
+    it('should postcodeChangeToBarcode："-"的位置，正确第6位 ', () =>{
+        const postcode = '13684-2796';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = '|:::||::||::||::|::|::|::|::|:||:::||:|:::||::|';
+
+        expect(barcode).toEqual(expected);
+    });
+
+    it('should postcodeChangeToBarcode："-"的位置，错误第5位 ', () =>{
+        const postcode = '0985-61234';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = 'Please enter the correct barcode';
+
+        expect(barcode).toEqual(expected);
+    });
+
+    it('should postcodeChangeToBarcode："-"的位置，错误第7位 ', () =>{
+        const postcode = '098526-234';
+
+        let allCodes = loadAllCodes();
+
+        let barcode = postcodeChangeToBarcode(postcode, allCodes);
+
+        const expected = 'Please enter the correct barcode';
+
+        expect(barcode).toEqual(expected);
+    });
+
+
+    it('should postcodeChangeToBarcode：出现字母', () =>{
         const postcode = 'A6s630';
 
         let allCodes = loadAllCodes();
@@ -501,32 +538,8 @@ describe('postnet', function () {
         expect(barcode).toEqual(expected);
     });
 
-    it('should postcodeChangeToBarcode', () =>{
+    it('should postcodeChangeToBarcode：出现其他字符', () =>{
         const postcode = 'f1534&84625';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '-5056-1234';
-
-        let allCodes = loadAllCodes();
-
-        let barcode = postcodeChangeToBarcode(postcode, allCodes);
-
-        const expected = 'Please enter the correct barcode';
-
-        expect(barcode).toEqual(expected);
-    });
-
-    it('should postcodeChangeToBarcode', () =>{
-        const postcode = '';
 
         let allCodes = loadAllCodes();
 
